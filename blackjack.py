a824b5c42fb305e6a58fcdca42d116cb7e228bd4
Nathan Vogt <merrick.vogt@gmail.com>
2023-08-15 14:44:02 -0400

    clean comments. added end scene.

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,6 @@
 # Write your blackjack game here.
 
 import random
-import math
 
 # class for an individual card
 class Card:
@@ -15,6 +14,7 @@ class Card:
 # class for the deck of cards
 class Deck:
     def __init__(self):
+        # 52 cards in random order
         self.card_deck = []
         suits = ['♠', '♥', '♦', '♣']
         values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
@@ -24,54 +24,40 @@ class Deck:
                 self.card_deck.append(card)
         
     def __str__(self):
-
         card_strings = [f"{card.value}{card.suit}" for card in self.card_deck]
         return ', '.join(card_strings)  # Join the card strings with commas
-
-        # 52 cards in random order
-        # create 52 cards, then short
+ 
+        # shuffle 52 cards in the deck
     def shuffle(self):
         random.shuffle(self.card_deck)
         return self.card_deck
 
 
-        # self.order = 'something'
-
-# class for the automated dealer
-# Dealer or Player could be child of the other (Dealer child of Player)
-class Dealer:
-    def __init__(self):
-        # attributes
-        self.hand_value = 0
-
-        # methods
-            # get 2 cards
-            # calculate value of hand
-            # "hit to get another card if value < 17"
-
+# class for the player. The dealer is a sub-class of player.
 class Player:
     def __init__(self, name='Nathan'):
     # attributes
         # Initialize an empty list to store cards
-        self.cards = [] 
+        self.cards = []
+        # Name of player 
         if name is None:
             self.name = input('What is your name? ')
         else:
             self.name = name
 
-    #methods
+    # methods
     def __str__(self):
         return self.name
 
+    # get dealt a new card
+    # used for initial deal and when a player "hits"
     def get_card(self, deck):
         # Remove top card from deck
         top_card = deck.card_deck.pop()
         # Add the card to the player's cards list
         self.cards.append(top_card)
 
-            # get 2 cards
-            # calculate value of hand
-            # prompt user to "hit" if they want
+    # option for a player to "hit" (get another card)
     def hit(self, deck):
         hit = input('Would you like to hit? [y/n]: ')
         if hit.lower() == 'y':
@@ -85,7 +71,7 @@ The total hand value is {self.hand_value}''')
         else:
             print(f'You chose to stay. Your hand value is {self.hand_value}')
             
-    
+    # calculates the total value of the player's hand
     @property
     def hand_value(self):
         total = 0
@@ -120,7 +106,7 @@ class Dealer(Player):
                 print(f'The dealer got a {self.cards[-1].value} of {self.cards[-1].suit}')
                 print(f'The dealer\'s hand value is now {self.hand_value}. The dealer busts.')
             else:
-                print(f''' The dealer got a {self.cards[-1].value} of {self.cards[-1].suit}
+                print(f'''    The dealer got a {self.cards[-1].value} of {self.cards[-1].suit}
 The dealer's hand value is {self.hand_value}''')
                 return self.hit(deck)
         else:
@@ -128,28 +114,47 @@ The dealer's hand value is {self.hand_value}''')
 
 class Game:
     def __init__(self):
-        # attributes
-            # name of game
-            # game over?
-            self.name = 'Blackjack'
-            self.game_over = False
+    # attributes
+        self.name = 'Blackjack'
+        self.game_over = False
             
-        # methods
-            # display player cards (2nd card and those after)
-
+    # methods
+    # returns the name of the game instead of the default of object's location
     def __str__(self):
         return self.name
     
+    # print introduction to game
+    # cards must be dealt first
     def game_intro(self):
         # introduce player to blackjack
-        print(f'''{self.player}, welcome to {self.name}. 
-You got a {self.player.cards[0].value} of {self.player.cards[0].suit} 
-and a {self.player.cards[1].value} of {self.player.cards[1].suit}
-The total hand value is {self.player.hand_value}''')
+        print(f'''{self.player}, welcome to {self.name}! 
+    You got a {self.player.cards[0].value} of {self.player.cards[0].suit} 
+    and a {self.player.cards[1].value} of {self.player.cards[1].suit}
+Your total hand value is {self.player.hand_value}''')
             
-        print(f'''The dealer\'s faceup card is a {self.dealer.cards[1].value} of {self.dealer.cards[1].suit}''')
-        print(f'The dealer\'s hidden card is a {self.dealer.cards[0].value} of {self.dealer.cards[0].suit}')
+        print(f'''    The dealer\'s faceup card is a {self.dealer.cards[1].value} of {self.dealer.cards[1].suit}''')
+        print(f'    The dealer\'s hidden card is a {self.dealer.cards[0].value} of {self.dealer.cards[0].suit}')
 
+    # ending scene depending on results of game
+    # 4 options.
+    def game_end(self):
+        if self.player.hand_value > 21:
+            print(f'{self.player.name} busts and loses this round.')
+        elif self.dealer.hand_value > 21:
+            print(f'{self.player.name} wins! The dealer busts.')
+        elif self.dealer.hand_value >= self.player.hand_value:
+            print(f'Dealer wins.')
+        else:
+            print(f'{self.player.name} wins!')
+        
+        play_again = input('Would you like to play again? [y/n]: ')
+        if play_again.lower() == 'y':
+            self.start_game()
+        else:
+            print('Great playing! See you next time.')
+
+
+    # game play!
     def start_game(self):
         # create instances of player and dealer
         self.player = Player()
@@ -173,14 +178,12 @@ The total hand value is {self.player.hand_value}''')
         self.game_intro()
 
         # give option for dealer and player to hit
-        self.dealer.hit(self.deck)
         self.player.hit(self.deck)
+        if self.player.hand_value <= 21:
+            self.dealer.hit(self.deck)
 
-        
-        
-        
-        
-    # def play_game(self):
+        # End Game Scene
+        self.game_end()
 
 
 new_game = Game()
